64834b0eedaa11e11f40ce17936d6d403e5de6ff
pieterbrandsen <pieterbrandsen2@gmail.com>
2025-02-24 16:21:09 +0100

    Corrected loop, use ticksinfile isntead of 100

--- UserTracker.HistoryFileTesterConsole/RoomHistory/HistoryFileCheker.cs
+++ UserTracker.HistoryFileTesterConsole/RoomHistory/HistoryFileCheker.cs
@@ -88,7 +88,7 @@ namespace UserTracker.Tests.RoomHistory
 
             if (roomData.TryGetValue("ticks", out JToken? jTokenTicks) && jTokenTicks is JObject jObjectTicks)
             {
-                for (int i = 0; i < 100; i++)
+                for (int i = 0; i < ConfigSettingsState.TicksInFile; i++)
                 {
                     long tickNumber = roomHistory.Base + i;
                     roomHistory.Tick = tickNumber;
