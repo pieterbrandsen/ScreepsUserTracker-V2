00865e95de9ecb1c26200457f2fa529d3fbd58b4
Pieter Brandsen <pieterbrandsen2@gmail.com>
2025-06-14 20:29:25 +0000

    Added new migrations

--- UserTrackerShared/Migrations/20250614202908_AddedOtherModels.Designer.cs
+++ UserTrackerShared/Migrations/20250614202908_AddedOtherModels.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UserTrackerShared.DBClients.TimeScaleDB;
@@ -11,9 +12,11 @@ using UserTrackerShared.DBClients.TimeScaleDB;
 namespace UserTrackerShared.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250614202908_AddedOtherModels")]
+    partial class AddedOtherModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
