9176250124c32a45aeb931e8575780027e293941
pieterbrandsen <pieterbrandsen2@gmail.com>
2026-01-22 21:30:35 +0100

    Fixed tests

--- UserTrackerConsole/Program.cs
+++ UserTrackerConsole/Program.cs
@@ -7,7 +7,6 @@ internal static class Program
     private static async Task<int> Main()
     {
         using var lifetime = new ConsoleLifetime();
-        var app = new ConsoleApp();
-        return await app.RunAsync(lifetime.Token);
+        return await ConsoleApp.RunAsync(lifetime.Token);
     }
 }
